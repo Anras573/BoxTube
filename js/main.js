7512d9c3788dc68163ad295a2e3c0efbba1204b8
Anders Bo Rasmussen <andersborasmussen@hotmail.com>
2015-05-13 23:40:14 +0200

    Forgot to commit js

--- js/main.js
+++ js/main.js
@@ -1,129 +1,151 @@
 var boxTube = angular.module('boxTube', []);
 
-boxTube.run(function () {
+boxTube.run(function() {
     var tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
 
-boxTube.service('youtubeService', ['$window', '$rootScope', function ($window, $rootScope) {
+boxTube.service('youtubeService', ['$window', '$rootScope', function($window, $rootScope) {
 
-        var service = this;
+    var service = this;
 
-        var youtube = {
-            ready: false,
-            player: null,
-            playerId: 'player',
-            videoId: '5NV6Rdv1a3I',
-            videoTitle: 'Daft Punk - Get Lucky (Official Audio) ft. Pharrell Williams, Nile Rodgers',
-            playerHeight: '480',
-            playerWidth: '640',
-            state: 'stopped'
-        };
+    var youtube = {
+        ready: false,
+        player: null,
+        playerId: 'player',
+        videoId: '5NV6Rdv1a3I',
+        videoTitle: 'Daft Punk - Get Lucky (Official Audio) ft. Pharrell Williams, Nile Rodgers',
+        playerHeight: '480',
+        playerWidth: '640',
+        state: 'stopped'
+    };
 
-        var results = [];
-        var playlist = [
-            {id: 'YqeW9_5kURI', title: 'Major Lazer & DJ Snake - Lean On (feat. MØ) (Official Music Video)'},
-            {id: 'X46t8ZFqUB4', title: 'Zedd - I Want You To Know ft. Selena Gomez'}
-        ];
+    var results = [];
+    var playlist = [{
+        id: 'YqeW9_5kURI',
+        title: 'Major Lazer & DJ Snake - Lean On (feat. MØ) (Official Music Video)'
+    }, {
+        id: 'X46t8ZFqUB4',
+        title: 'Zedd - I Want You To Know ft. Selena Gomez'
+    }];
+    var history = [{
+        id: youtube.videoId,
+        title: youtube.videoTitle
+    }];
 
-        $window.onYouTubeIframeAPIReady = function () {
-            youtube.player = new YT.Player(youtube.playerId, {
-                height: youtube.playerHeight,
-                width: youtube.playerWidth,
-                events: {
-                    'onReady': onPlayerReady,
-                    'onStateChange': onPlayerStateChange
-                }
+    $window.onYouTubeIframeAPIReady = function() {
+        youtube.player = new YT.Player(youtube.playerId, {
+            height: youtube.playerHeight,
+            width: youtube.playerWidth,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+        $rootScope.$apply();
+    };
+
+    function onPlayerReady(event) {
+        youtube.player.cueVideoById(youtube.videoId);
+        event.target.playVideo();
+    }
+
+    function onPlayerStateChange(event) {
+        if (event.data === YT.PlayerState.PLAYING) {
+            youtube.state = 'playing';
+        } else if (event.data === YT.PlayerState.PAUSED) {
+            youtube.state = 'paused';
+        } else if (event.data === YT.PlayerState.ENDED) {
+            youtube.state = 'ended';
+            service.playNextVideo(playlist[0]);
+        }
+        $rootScope.$apply();
+    }
+
+    this.playNextVideo = function(video) {
+        youtube.player.loadVideoById(video.id);
+        youtube.videoId = video.id;
+        youtube.videoTitle = video.title;
+        history.push(video);
+        playlist.splice(0, 1);
+    };
+
+    this.listResults = function(data) {
+        results.length = 0;
+        for (var i = 0; i < data.items.length; i++) {
+            results.push({
+                id: data.items[i].id.videoId,
+                title: data.items[i].snippet.title,
+                description: data.items[i].snippet.description,
+                thumbnail: data.items[i].snippet.thumbnails.default.url,
+                author: data.items[i].snippet.channelTitle
             });
-            $rootScope.$apply();
-        };
-
-        function onPlayerReady(event) {
-            youtube.player.cueVideoById(youtube.videoId);
-            event.target.playVideo();
         }
+        return results;
+    };
 
-        function onPlayerStateChange(event) {
-            if (event.data === YT.PlayerState.PLAYING) {
-                youtube.state = 'playing';
-            } else if (event.data === YT.PlayerState.PAUSED) {
-                youtube.state = 'paused';
-            } else if (event.data === YT.PlayerState.ENDED) {
-                youtube.state = 'ended';
-                service.playNextVideo(playlist[0]);
-            }
-            $rootScope.$apply();
-        }
+    this.deleteVideoFromPlaylist = function(index) {
+        playlist.splice(index, 1);
+    };
 
-        this.playNextVideo = function (video) {
-            youtube.player.loadVideoById(video.id);
-            youtube.videoId = video.id;
-            youtube.videoTitle = video.title;
-            playlist.splice(0, 1);
-        };
+    this.deleteVideoFromHistory = function(index) {
+        history.splice(index, 1);
+    };
 
-        this.listResults = function (data) {
-            results.length = 0;
-            for (var i = 0; i < data.items.length; i++) {
-                results.push({
-                    id: data.items[i].id.videoId,
-                    title: data.items[i].snippet.title,
-                    description: data.items[i].snippet.description,
-                    thumbnail: data.items[i].snippet.thumbnails.default.url,
-                    author: data.items[i].snippet.channelTitle
-                });
-            }
-            return results;
-        };
+    this.playVideoFromPlaylist = function(index) {
+        var video = playlist[index];
+        youtube.player.loadVideoById(video.id);
+        youtube.videoId = video.id;
+        youtube.videoTitle = video.title;
+        history.push(video);
+        service.deleteVideoFromPlaylist(index);
+    };
 
-        this.deleteVideoFromList = function (video) {
-            for (var i = 0; i < playlist.length; i++) {
-                if (playlist[i].id === video.id) {
-                    playlist.splice(i, 1);
-                    break;
-                }
-            }
-        };
+    this.playVideoFromHistory = function(index) {
+        var video = history[index];
+        youtube.player.loadVideoById(video.id);
+        youtube.videoId = video.id;
+        youtube.videoTitle = video.title;
+        history.push(video);
+    };
 
-        this.playVideo = function (video) {
-            youtube.player.loadVideoById(video.id);
-            youtube.videoId = video.id;
-            youtube.videoTitle = video.title;
-            service.deleteVideoFromList(video);
-        };
+    this.queueVideo = function(video) {
+        playlist.push(video);
+    };
 
-        this.queueVideo = function (video) {
-            playlist.push(video);
-        };
+    this.getYoutube = function() {
+        return youtube;
+    };
 
-        this.getYoutube = function () {
-            return youtube;
-        };
+    this.getResults = function() {
+        return results;
+    };
 
-        this.getResults = function () {
-            return results;
-        };
+    this.getPlaylist = function() {
+        return playlist;
+    };
 
-        this.getPlaylist = function () {
-            return playlist;
-        };
+    this.getHistory = function() {
+        return history;
+    };
 
-    }]);
+}]);
 
-boxTube.controller('youtubeController', ['$scope', '$http', 'youtubeService', function ($scope, $http, youtubeService) {
+boxTube.controller('youtubeController', ['$scope', '$http', 'youtubeService', function($scope, $http, youtubeService) {
 
-        init();
+    init();
 
-        function init() {
-            $scope.youtube = youtubeService.getYoutube();
-            $scope.results = youtubeService.getResults();
-            $scope.playlist = youtubeService.getPlaylist();
-        }
+    function init() {
+        $scope.youtube = youtubeService.getYoutube();
+        $scope.results = youtubeService.getResults();
+        $scope.playlist = youtubeService.getPlaylist();
+        $scope.history = youtubeService.getHistory();
+    }
 
-        $scope.search = function () {
-            $http.get('https://www.googleapis.com/youtube/v3/search', {
+    $scope.search = function() {
+        $http.get('https://www.googleapis.com/youtube/v3/search', {
                 params: {
                     key: 'SECRET-YOUTUBE-API-KEY-HERE',
                     type: 'video',
@@ -133,25 +155,36 @@ boxTube.controller('youtubeController', ['$scope', '$http', 'youtubeService', fu
                     q: this.request
                 }
             })
-                .success(function (data) {
-                    youtubeService.listResults(data);
-                })
-                .error(function (e) {
-                    console.log(e);
-                });
-        };
+            .success(function(data) {
+                youtubeService.listResults(data);
+            })
+            .error(function(e) {
+                console.log(e);
+            });
+    };
 
-        $scope.queue = function (result) {
-            var video = {id: result.id, title: result.title};
-            youtubeService.queueVideo(video);
+    $scope.queue = function(result) {
+        var video = {
+            id: result.id,
+            title: result.title
         };
+        youtubeService.queueVideo(video);
+    };
 
-        $scope.delete = function (video) {
-            youtubeService.deleteVideoFromList(video);
-        };
+    $scope.delete = function(index) {
+        youtubeService.deleteVideoFromPlaylist(index);
+    };
 
-        $scope.playVideo = function (video) {
-            youtubeService.playVideo(video);
-        };
+    $scope.deleteFromHistory = function(index) {
+        youtubeService.deleteVideoFromHistory(index);
+    };
 
-    }]);
+    $scope.playVideoFromPlaylist = function(index) {
+        youtubeService.playVideoFromPlaylist(index);
+    };
+
+    $scope.playVideoFromHistory = function(index) {
+        youtubeService.playVideoFromHistory(index);
+    };
+
+}]);
